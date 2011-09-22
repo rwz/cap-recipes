2b7d5be59bcf535754e280aa3c5e5aa5d2860307
Pavel Pravosud <rwz@duckroll.ru>
2011-09-22 17:20:54 +0700

    wheneverize

--- lib/cap_recipes/tasks/whenever/manage.rb
+++ lib/cap_recipes/tasks/whenever/manage.rb
@@ -2,7 +2,7 @@ Capistrano::Configuration.instance(true).load do
   namespace :whenever do
     desc "Update the crontab file"
     task :update_crontab, :roles => :db do
-      run "cd #{release_path} && whenever --update-crontab #{application}"
+      run "cd #{release_path} && RAILS_ENV=#{rails_env} bundle exec whenever --update-crontab #{application}_#{rails_env}"
     end
   end
 end
