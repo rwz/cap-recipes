298b83ec0090a1fa36b2a18bcea2afe220bf7398
Pavel Pravosud <rwz@duckroll.ru>
2011-09-22 18:03:35 +0700

    delayed job

--- lib/cap_recipes/tasks/delayed_job/manage.rb
+++ lib/cap_recipes/tasks/delayed_job/manage.rb
@@ -1,31 +1,27 @@
 require File.expand_path(File.dirname(__FILE__) + '/../utilities')
 
 Capistrano::Configuration.instance(true).load do
-  set :delayed_script_path, "#{current_path}/script/delayed_job"
   set :delayed_job_role, :app
 
   namespace :delayed_job do
     desc "Start delayed_job process"
     task :start, :roles => delayed_job_role do
       utilities.with_role(delayed_job_role) do
-        try_sudo "RAILS_ENV=#{rails_env} ruby #{delayed_script_path} start"
+        try_sudo "cd #{current_path}; RAILS_ENV=#{rails_env} script/delayed_job start"
       end
     end
 
     desc "Stop delayed_job process"
     task :stop, :roles => delayed_job_role do
       utilities.with_role(delayed_job_role) do
-        try_sudo "RAILS_ENV=#{rails_env} ruby #{delayed_script_path} stop"
+                try_sudo "cd #{current_path}; RAILS_ENV=#{rails_env} script/delayed_job stop"
       end
     end
 
     desc "Restart delayed_job process"
     task :restart, :roles => delayed_job_role do
       utilities.with_role(delayed_job_role) do
-        delayed_job.stop
-        sleep(4)
-        try_sudo "killall -s TERM delayed_job; true"
-        delayed_job.start
+        try_sudo "cd #{current_path}; RAILS_ENV=#{rails_env} script/delayed_job restart"
       end
     end
   end
